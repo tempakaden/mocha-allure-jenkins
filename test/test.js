7b0777af018f72e611f0802071a33955798f2b53
Denis <denis.sergeev@ax-team.com>
2022-05-16 15:29:14 +0300

    debug

--- test/test.js
+++ test/test.js
@@ -2,11 +2,11 @@ var assert = require('assert');
 require("mocha-allure-reporter");
 
 describe('Микросервис обработки анкет партнеров', function () {
-  const testStep = allure.createStep("#1 initial",()=> {
+  const testStep = allure.createStep("initial",()=> {
     //something
     assert.equal([1, 2, 3].indexOf(4), -1);
   })
-    it('правильный /POST запрос на создание анкеты от партнера такого-то должен быть принят', function () {
+    it('#1 правильный /POST запрос на создание анкеты от партнера такого-то должен быть принят', function () {
       testStep();
     });
 });
