4c43563b55e36116e80cfa5a4be2d1250c5c80cd
Denis <denis.sergeev@ax-team.com>
2022-10-24 12:03:03 +0300

    update tests

---
+++ test/allure.test.js
@@ -0,0 +1,259 @@
+var reporter=require('allure-mocha/runtime');
+describe('Фича', function () {
+
+    it('тест номер 1', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','1');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 2', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','2');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 3', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','3');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 4', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','4');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 1', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','1');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 5', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','5');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 6', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','6');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 7', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','7');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 8', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','8');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 9', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','9');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 10', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','10');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 11', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','11');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 12', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','12');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 13', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','13');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+    it('тест номер 14', function () {
+        reporter.allure.feature('Фича супер такая-то номер 1');
+        reporter.allure.issue("jira", "https://jira.somecorp.ru");
+        reporter.allure.tag("smoke");
+        reporter.allure.label('AS_ID','14');
+
+        reporter.allure.logStep('step1',"passed");
+
+        reporter.allure.logStep('step2',"passed");
+
+        reporter.allure.logStep('step3',"passed");
+
+        reporter.allure.logStep('step4',"passed");
+
+        reporter.allure.logStep('step5',"passed");
+
+    });
+});
